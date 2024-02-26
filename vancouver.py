d1f6c5a898f1ae52a83941f09af8aa7559b4fe94
Blake Frederick <bf@six12creative.com>
2024-02-25 21:28:19 -0800

    do extra things to make extra visuals

--- vancouver.py
+++ vancouver.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 file_path = 'street-trees.xlsx' # opendata.vancouver.ca
 data_frame = pd.read_excel(file_path) # thanks pandas
 
-# # Trees by Street
+# Trees by Street
 street_tree_count = data_frame['STD_STREET'].value_counts().head(25) # just the top 25 streets
 street_tree_count.plot(kind='bar', figsize=(10, 6), title='Top 25 Tree Streets')
 plt.xlabel('Street')
@@ -13,7 +13,7 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# # Trees by Species
+# Trees by Species
 species_tree_count = data_frame['COMMON_NAME'].value_counts().head(25)
 species_tree_count.plot(kind='bar', figsize=(15, 8), color='forestgreen', title='Top 25 Tree Species')
 plt.xlabel('Species')
@@ -24,14 +24,25 @@ plt.tight_layout()
 plt.show()
 
 # Trees Planted by Year
+
 data_frame['DATE_PLANTED'] = pd.to_datetime(data_frame['DATE_PLANTED'])
-data_frame['YEAR'] = data_frame['DATE_PLANTED'].dt.year # count trees by year
+data_frame = data_frame.dropna(subset=['DATE_PLANTED'])  # rm NAs so we can convert to int in next step
+data_frame['YEAR'] = data_frame['DATE_PLANTED'].dt.year.astype(int) # otherwise it'll be a float like 2009.0
 yearly_counts = data_frame.groupby('YEAR').size()
+
 plt.figure(figsize=(15, 8))
-yearly_counts.plot(kind='bar', color='indigo') 
-plt.title('Trees Planted by Year')
-plt.xlabel('Year Planted')
-plt.ylabel('Number of Trees Planted')
-plt.xticks(rotation=45)
-plt.tight_layout() 
+plt.plot(yearly_counts.index.astype(str), yearly_counts, color='indigo', linewidth=2)  # this one's a line graph
+plt.title('Trees Planted by Year', fontweight='bold')
+plt.xlabel('Year Planted', fontweight='bold')
+plt.ylabel('Number of Trees Planted', fontweight='bold')
+plt.xticks(rotation=45, fontweight='bold')
+plt.yticks(fontweight='bold')
+plt.grid(True)
+
+# make the graph lines thicker
+ax = plt.gca()
+for axis in ['top','bottom','left','right']:
+    ax.spines[axis].set_linewidth(2)
+
+plt.tight_layout()
 plt.show()
