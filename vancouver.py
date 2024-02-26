0e9d546bbf54174649dbcc68cf38aa9a44514610
Blake Frederick <bf@six12creative.com>
2024-02-25 20:47:36 -0800

    another graph appears

--- vancouver.py
+++ vancouver.py
@@ -4,10 +4,20 @@ import matplotlib.pyplot as plt
 file_path = 'street-trees.xlsx' # opendata.vancouver.ca
 data_frame = pd.read_excel(file_path) # thanks pandas
 
-street_tree_count = data_frame['STD_STREET'].value_counts().head(25) # just the top 25
+# Trees by Street
+street_tree_count = data_frame['STD_STREET'].value_counts().head(25) # just the top 25 streets
 street_tree_count.plot(kind='bar', figsize=(10, 6), title='Top 25 Tree Streets')
 plt.xlabel('Street')
 plt.ylabel('Number of Trees')
 plt.xticks(rotation=45)
-plt.tight_layout() 
+plt.tight_layout()
+plt.show()
+
+# Trees by Species
+species_tree_count = data_frame['COMMON_NAME'].value_counts().head(25)
+species_tree_count.plot(kind='bar', figsize=(10, 6), title='Top 25 Tree Species')
+plt.xlabel('Species')
+plt.ylabel('Number of Trees')
+plt.xticks(rotation=45)
+plt.tight_layout()
 plt.show()
